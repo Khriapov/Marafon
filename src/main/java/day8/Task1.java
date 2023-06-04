85d2a7bc5e2cb9235c83b8ae8cc660a834993e17
Khriapov Sergei <s.khriapov@mail.ru>
2023-06-04 07:00:45 +0300

    add day 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,21 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        long startTimeString = System.currentTimeMillis();
+        String string = "";
+        for (int i = 0; i <= 20000; i++) {
+            string += i + " ";
+        }
+        long stopTimeString = System.currentTimeMillis();
+        System.out.println();
+        System.out.println("Длительность работы в мс (String): " + (stopTimeString - startTimeString));
+        System.out.println();
+        long startTimeStringBulder = System.currentTimeMillis();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i <= 20000; i++) {
+            stringBuilder.append(i).append(" ");
+        }
+        long stopTimeStringBulder = System.currentTimeMillis();
+        System.out.println("Длительность работы в мс (StringBulder): " + (stopTimeStringBulder - startTimeStringBulder));
     }
 }

bf731805f950a6bda39ce96c8cc85d3d92e0c1b4
Khriapov Sergei <s.khriapov@mail.ru>
2023-08-02 13:28:23 +0300

    add day 13

--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -2,6 +2,27 @@ package day13;
 
 public class Task1 {
     public static void main(String[] args) {
+        User userOne = new User("Сергей Сергеевич");
+        User userTwo = new User("Дмитрий Дмитриевич");
+        User userFree = new User("Василий Васильевич");
+
+        userOne.sendMessage(userTwo, "Привет!");
+        userOne.sendMessage(userTwo, "Как твои дела?");
+
+        userTwo.sendMessage(userOne, "Привет!");
+        userTwo.sendMessage(userOne, "Дела отлично");
+        userTwo.sendMessage(userOne, "Ты завтра свободен?");
+
+        userFree.sendMessage(userOne, "Привет!");
+        userFree.sendMessage(userOne, "Как дела?");
+        userFree.sendMessage(userOne, "Разговаривал со вторым насчет завтра?");
+
+        userOne.sendMessage(userFree, "Привет!");
+        userOne.sendMessage(userFree, "Дела замечательно");
+        userOne.sendMessage(userFree, "Он только написал");
+
+        userFree.sendMessage(userOne, "Как догворитесь - напиши мне!");
+        MessageDatabase.showDialog(userOne, userFree);
 
     }
 }
